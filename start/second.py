4664d04600062b43755cdebb3fc8953ae22b5aee
Dana <159054921+Dunnaya@users.noreply.github.com>
2025-01-01 21:46:13 +0200

    Update second.py

--- start/second.py
+++ start/second.py
@@ -1,1 +1,28 @@
 #conditions (if/elif/else) and logical operators
+
+def age_check():
+    while True:
+        try:
+            age = int(input("Enter your age: "))
+            break
+        except ValueError:
+            print("Eblo? Enter int.")
+
+    if age < 0:
+        print("Autist?")
+    elif age < 18:
+        print("Soberi penal")
+    elif age <= 70:
+        print("You're boring now")
+    else:
+        print("Hahahah plesenb")
+
+while True:
+    age_check()
+
+    answer = input("Do you want to check it again? (y/n)").strip().lower()
+    if answer == 'n':
+        break;
+    elif answer != 'y':
+        print("Pridurok")
+        break
