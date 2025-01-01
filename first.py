92b2b10f577c0013c563b3d9ebbe7bdc631e3112
Dana <holydunnaya@gmail.com>
2025-01-01 20:20:41 +0200

    again

---
+++ first.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+#input processing
+while True:
+    try:
+        fav_num = int(input("Enter your favorite number: "))
+        break
+    except ValueError:
+        print("Eblan? Enter int value.")
+
+print("Hello " + name)
+print("In 10 years you will be "  + str(age + 10))
+print(f"Your fav number doubled is {fav_num*2}")
